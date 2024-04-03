a79741d3c71ca450dde7635fa8b13753a5a397aa
NeugeMa <mariana.n.dourado@gmail.com>
2024-04-02 21:15:35 -0300

    inciando aula de js

---
+++ aula03-Js/script.js
@@ -0,0 +1,24 @@
+// Comandos em JS
+// alert("Olá mundo!")                      // Fazendo um alerta 
+// prompt("Digite seu nome: ")              // Fazendo um input 
+// confirm("Você deseja sair?")             // Mensagens para confirmar 
+
+// console.log("Mensagem no console")
+
+// UTILIZANDO VARIÁVEIS 
+// var nome = 'Mari'
+// var idade = 19 
+// console.log(nome)
+// console.log(idade)
+
+// console.log(nome+idade)                  // Juntanto variáveis  
+
+// Pegando informações: 
+// var nome = prompt("Digite seu nome: ")      // Entrada 
+// console.log(nome)                           // Saída
+// console.log("Olá, " + nome + ", seja bem-vindo(a)")
+
+var nome = prompt("Digite seu nome: ")
+var idade = prompt("Digite sua idade: ")
+var profissao = prompt("Digite sua profissão: ")
+console.log("Olá " + nome + ", você possui " + idade + " anos e sua profissão é: " + profissao)
